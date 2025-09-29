ec89d866aa81e75d63dd726782c8c099e3bfb43c
Fykn <fernandokodagawa@gmail.com>
2025-09-29 17:13:35 -0300

    Update Jogo da Velha Mas Nao Feito Por Uma Anta.cpp

--- Jogo da Velha Mas Nao Feito Por Uma Anta.cpp
+++ Jogo da Velha Mas Nao Feito Por Uma Anta.cpp
@@ -2,24 +2,98 @@
 #include <string>
 using namespace std;
 
-int VerificaJgda(int jgda[], int i){
+void EscolheJgda(int jgda[], int i){
     string mJ1 = "Jogador 1: ";
     string mJ2 = "Jogador 2: ";
-	int jgdaInvalida = 0;
-	for(int j = 1; j <= i; j++){
-		if(jgda[i] < 1 || jgda[i] > 9 || jgda[i] == jgda[i-j]){
-			do{
-				jgdaInvalida = 1;
-				cout << "Jogada invalida, escolha outra.";
-				if(i % 2 == 0) 
-                    cout << endl << mJ1;
+    bool jgdaInvalida;
+    do{
+        jgdaInvalida = 0;
+        if(i % 2 == 0)
+            cout << mJ1;
+        else
+            cout << mJ2;
+        cin >> jgda[i];
+        if(jgda[i] < 1 || jgda[i] > 9){
+            jgdaInvalida = 1;
+            cout << "Jogada invalida, tente novamente." << endl;
+        }
+        else{
+            for(int j = 1; j <= i; j++){
+                if(jgda[i] == jgda[i-j]){
+                    jgdaInvalida = 1;
+                    cout << "Jogada invalida, tente novamente." << endl;
+                }
+            }
+        }
+    }while(jgdaInvalida == 1);
+}
+
+bool Vitoria(int i, string p[], string j_){
+    string mJ1 = "Jogador 1 venceu.";
+    string mJ2 = "Jogador 2 venceu.";
+    bool Venceu = 0;
+        for(int x = 0; x < 4; x += 3){
+            if(p[x] == "_" + j_ + "_" && p[x+1] == p[x] && p[x+2] == p[x]){
+                Venceu = 1;
+                if(i % 2 == 0)
+                    cout << mJ1;
                 else
-                    cout << endl << mJ2;
-				cin >> jgda[i];
-			}while(jgda[i] < 1 || jgda[i] > 9 || jgda[i] == jgda[i-j]);
-		}
-	}
-	return jgdaInvalida;
+                    cout << mJ2;
+            }
+        }
+        for(int y = 0; y < 4; y++){
+            if(p[y] == "_" + j_ + "_" && p[y+3] == p[y] && p[y+6] == " " + j_ + " "){
+                Venceu = 1;
+                if(i % 2 == 0)
+                    cout << mJ1;
+                else
+                    cout << mJ2;
+            }
+        }
+        for(int z = 1; z < 4; z += 2){
+            if(p[z-1] == "_" + j_ + "_" && p[4] == p[z-1] && p[9-z] == " " + j_ + " "){
+                Venceu = 1;
+                if(i % 2 == 0)
+                    cout << mJ1;
+                else
+                    cout << mJ2;
+            }
+        }
+        if(p[6] == " " + j_ + " " && p[7] == p[6] && p[8] == p[6]){
+            Venceu = 1;
+            if(i % 2 == 0)
+                cout << mJ1;
+            else
+                cout << mJ2;
+        }
+    return Venceu;
+}
+
+string JgdaJ_(int jgda[], int i, string p[], string j_, string T){ 
+    if(jgda[i] == 1)
+        p[0] = "_" + j_ + "_";
+    else if(jgda[i] == 2)
+        p[1] = "_" + j_ + "_";
+    else if(jgda[i] == 3)
+        p[2] = "_" + j_ + "_";
+    else if(jgda[i] == 4)
+        p[3] = "_" + j_ + "_";
+    else if(jgda[i] == 5)
+        p[4] = "_" + j_ + "_";
+    else if(jgda[i] == 6)
+        p[5] = "_" + j_ + "_";
+    else if(jgda[i] == 7)
+        p[6] = " " + j_ + " ";
+    else if(jgda[i] == 8)
+        p[7] = " " + j_ + " ";
+    else
+        p[8] = " " + j_ + " ";
+
+    string l1 = p[0] + '|' + p[1] + '|' + p[2];
+    string l2 = p[3] + '|' + p[4] + '|' + p[5];
+    string l3 = p[6] + '|' + p[7] + '|' + p[8];
+    T = l1 + '\n' + l2 + '\n' + l3;
+    return T;
 }
 
 int main(){
@@ -27,14 +101,15 @@ int main(){
 	string l1 = "_1_|_2_|_3_";
 	string l2 = "_4_|_5_|_6_";
 	string l3 = " 7 | 8 | 9 ";
-    string p1, p2, p3, p4, p5, p6, p7, p8, p9;
-    p1 = p2 = p3 = p4 = p5 = p6 = "___";
-    p7 = p8 = p9 = "   ";
+	string T = l1 + '\n' + l2 + '\n' + l3;
+    string p[9];
+    p[0] = p[1] = p[2] = p[3] = p[4] = p[5] = "___";
+    p[6] = p[7] = p[8] = "   ";
 	cout << "Jogo da Velha" << endl;
-	cout << l1 << endl << l2 << endl << l3 << endl << endl;
+	cout << T << endl << endl;
 
 	//Jogadas e jogadores
-    int jgda[8];
+    int jgda[9];
     string mJ1 = "Jogador 1: ";
 	string mJ2 = "Jogador 2: ";
     string j1, j2;
@@ -43,7 +118,7 @@ int main(){
     cin >> j1;
     
     while(j1 != "X" && j1 != "x" && j1 != "O" && j1 != "o"){
-        cout << "Invalido, tente novamente" << endl << mJ1;
+        cout << "Invalido, tente novamente." << endl << mJ1;
         cin >> j1;
     }
     if(j1 == "X" || j1 == "x"){
@@ -54,111 +129,21 @@ int main(){
         j1 = "o";
         j2 = "x";
     }
-
     cout << mJ2 << j2 << endl << endl;
-
-    //1º Jogada
-    cout << mJ1;
-    cin >> jgda[0];
-    while(jgda[0] < 1 || jgda[0] > 9){
-        cout << "Jogada invalida, tente novamente" << endl << mJ1;
-        cin >> jgda[0];
-    }
-
-    if(jgda[0] == 1)
-        p1 = "_" + j1 + "_";
-    else if(jgda[0] == 2)
-        p2 = "_" + j1 + "_";
-    else if(jgda[0] == 3)
-        p3 = "_" + j1 + "_";
-    else if(jgda[0] == 4)
-        p4 = "_" + j1 + "_";
-    else if(jgda[0] == 5)
-        p5 = "_" + j1 + "_";
-    else if(jgda[0] == 6)
-        p6 = "_" + j1 + "_";
-    else if(jgda[0] == 7)
-        p7 = " " + j1 + " ";
-    else if(jgda[0] == 8)
-        p8 = " " + j1 + " ";
-    else
-        p9 = " " + j1 + " ";
-
-    l1 = p1 + '|' + p2 + '|' + p3;
-    l2 = p4 + '|' + p5 + '|' + p6;
-    l3 = p7 + '|' + p8 + '|' + p9;
-    cout << l1 << endl << l2 << endl << l3 << endl;
-
-    //Demais Jogadas
-    for(int i = 2; i < 5; i += 2){ //Jogador 1
-        for(int j = 1; j < 5; j += 2){ //Jogador 2
-                cout << mJ2;
-                cin >> jgda[j];
-                if(VerificaJgda(jgda, j) == 1){
-                    do{
-                        VerificaJgda(jgda, j);
-                    }while(VerificaJgda(jgda, j) == 1);
-                }
-
-                if(jgda[j] == 1)
-                    p1 = "_" + j2 + "_";
-                else if(jgda[j] == 2)
-                    p2 = "_" + j2 + "_";
-                else if(jgda[j] == 3)
-                    p3 = "_" + j2 + "_";
-                else if(jgda[j] == 4)
-                    p4 = "_" + j2 + "_";
-                else if(jgda[j] == 5)
-                    p5 = "_" + j2 + "_";
-                else if(jgda[j] == 6)
-                    p6 = "_" + j2 + "_";
-                else if(jgda[j] == 7)
-                    p7 = " " + j2 + " ";
-                else if(jgda[j] == 8)
-                    p8 = " " + j2 + " ";
-                else if(jgda[j] == 9)
-                    p9 = " " + j2 + " ";
-                else{
-                    cout << "Jogada invalida, tente novamente" << endl << mJ2;
-                    cin >> jgda[j];
-                }
-
-                l1 = p1 + '|' + p2 + '|' + p3;
-                l2 = p4 + '|' + p5 + '|' + p6;
-                l3 = p7 + '|' + p8 + '|' + p9;
-                cout << l1 << endl << l2 << endl << l3 << endl << endl;
-
-                cout << mJ1;
-                cin >> jgda[i];
-                if(VerificaJgda(jgda, i) == 1){
-                    do{
-                        VerificaJgda(jgda, i);
-                    }while(VerificaJgda(jgda, i) == 1);
-                }
-
-                if(jgda[i] == 1)
-                    p1 = "_" + j1 + "_";
-                else if(jgda[i] == 2)
-                    p2 = "_" + j1 + "_";
-                else if(jgda[i] == 3)
-                    p3 = "_" + j1 + "_";
-                else if(jgda[i] == 4)
-                    p4 = "_" + j1 + "_";
-                else if(jgda[i] == 5)
-                    p5 = "_" + j1 + "_";
-                else if(jgda[i] == 6)
-                    p6 = "_" + j1 + "_";
-                else if(jgda[i] == 7)
-                    p7 = " " + j1 + " ";
-                else if(jgda[i] == 8)
-                    p8 = " " + j1 + " ";
-                else
-                    p9 = " " + j1 + " ";
-
-                l1 = p1 + '|' + p2 + '|' + p3;
-                l2 = p4 + '|' + p5 + '|' + p6;
-                l3 = p7 + '|' + p8 + '|' + p9;
-                cout << l1 << endl << l2 << endl << l3 << endl << endl;
+	
+	//Jogadas
+    for(int i = 0; i < 9; i++){
+        if(i % 2 == 0){
+            EscolheJgda(jgda, i);
+            cout << JgdaJ_(jgda, i, p, j1, T) << endl << endl;
+            if(Vitoria(i, p, j1) == 1)
+                return 0;
         }
-    }  
+        else{
+            EscolheJgda(jgda, i);
+            cout << JgdaJ_(jgda, i, p, j2, T) << endl << endl;
+            if(Vitoria(i, p, j2) == 1)
+                return 0;
+        }
+    }
 }
